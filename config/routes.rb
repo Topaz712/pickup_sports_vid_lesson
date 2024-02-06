90ea5d46afe7a53d546c96fa7180cecb14d123dd
Topaz712 <tzam712@gmail.com>
2024-02-05 21:57:41 -0600

    ADDED: show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # localhost:3000/users does not work for me, use http://127.0.0.1:3000/users
+  get '/users', to: 'users#index'
 
+  # http://127.0.0.1:3000/users/1
+  get '/users/:id', to: 'users#show'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
