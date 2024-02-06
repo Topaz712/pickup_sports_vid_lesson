d1f836419e59f87259f3646564d3eed283a254fc
Topaz712 <tzam712@gmail.com>
2024-02-06 00:43:16 -0600

    ADDED: create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,18 @@ class UsersController < ApplicationController
 
     render json: user, status: 200
   end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name, :last_name)
+  end
 end
