5bba5bbe6325ad5c5bcc6e4367dc38e2b5a31cf8
Topaz712 <tzam712@gmail.com>
2024-02-07 18:49:53 -0600

    ADDED: update action w/ defined instance variable

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # http://127.0.0.1:3000/users
   post '/users', to: 'users#create'
 
+  # http://127.0.0.1:3000/users/1
+  put '/users/:id', to: 'users#update'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
