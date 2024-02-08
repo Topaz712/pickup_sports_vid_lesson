5b228db14cfcbd6c937f8a2b11d50800ba92c28d
Topaz712 <tzam712@gmail.com>
2024-02-07 21:45:50 -0600

    ADDED: got user posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,13 @@ before_action :set_user, only: [:show, :update, :destroy]
     end
   end
 
+  def posts_index
+    user = User.find(params[:user_id])
+    user_posts = @user.posts
+
+    render json: user_posts, status: :ok
+  end
+
   private
 
   def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
 Rails.application.routes.draw do
   # localhost:3000/users does not work for me, use http://127.0.0.1:3000/users
-  get '/users', to: 'users#index'
+  # get '/users', to: 'users#index'
 
-  # http://127.0.0.1:3000/users/1
-  get '/users/:id', to: 'users#show'
+  # # http://127.0.0.1:3000/users/1
+  # get '/users/:id', to: 'users#show'
 
-  # http://127.0.0.1:3000/users
-  post '/users', to: 'users#create'
+  # # http://127.0.0.1:3000/users
+  # post '/users', to: 'users#create'
 
-  # http://127.0.0.1:3000/users/1
-  put '/users/:id', to: 'users#update'
+  # # http://127.0.0.1:3000/users/1
+  # put '/users/:id', to: 'users#update'
 
-  # http://127.0.0.1:3000/users/1
-  delete '/users/:id', to: 'users#destroy'
+  # # http://127.0.0.1:3000/users/1
+  # delete '/users/:id', to: 'users#destroy'
+
+  resources :users do
+    get 'posts', to: "users#posts_index"
+  end
+
+  resources :posts
+  
+  end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
