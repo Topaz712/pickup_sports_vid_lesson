76edd754cfc7b387b32474ea575faf3063e373ff
Topaz712 <tzam712@gmail.com>
2024-02-18 18:15:10 -0600

    identified current user through requests

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Events", type: :request do
     let(:sport) { create(:sport) }
 
     before do
-      event_attributes = attributes_for(:event, user_id: user.id, sport_ids: [sport.id])
+      event_attributes = attributes_for(:event, sport_ids: [sport.id])
       post "/events", params: event_attributes, headers: { Authorization: "Bearer #{token}" }
     end
 
