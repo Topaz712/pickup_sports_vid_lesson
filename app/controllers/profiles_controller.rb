a643ceb15750d1665ddf99fdc7a0b843d098a401
Topaz712 <tzam712@gmail.com>
2024-02-18 19:57:37 -0600

    uncommented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  # before_action :authenticate_request
+  before_action :authenticate_request
   
   def show
     user = User.find_by(username: params[:username])
